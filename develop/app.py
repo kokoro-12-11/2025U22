daf00f4fd16f58f651ea8dbc44c4f8c763f87f68
sorato0714 <144749251+sorato0714@users.noreply.github.com>
2025-07-29 15:42:05 +0900

    250729/1541
    topページの追加

--- develop/app.py
+++ develop/app.py
@@ -43,6 +43,10 @@ def index():
 def login():
   return render_template('login.html')
 
+@app.route('/top')
+def top():
+  return render_template('top.html')
+
 @app.route('/register')
 def register():
   return render_template('register.html')
